60e7c78620cc7b427554a7949581b233450b38c2
Dougal Seeley <dougal.seeley@sky.uk>
2018-11-06 13:26:01 +0000

    Mount disks, decouple network/subnet creation, update GCE modules + Initialise and mount volumes. + Decouple network/subnet creation. + Update GCE to use latest modules (allow physical disk attachment) + Fixes for empty 'dns_server'

--- .vaultpass-client.py
+++ .vaultpass-client.py
@@ -7,13 +7,17 @@ parser = argparse.ArgumentParser()
 parser.add_argument("--vault-id", type=str)
 args = parser.parse_args()
 
-if args.vault_id is None or args.vault_id == "None" or args.vault_id == "default" or args.vault_id == "all":
+if args.vault_id is None or args.vault_id == "None" or args.vault_id == "default":
+    if os.environ.get('VAULT_PASSWORD_ALL') is not None and os.environ.get('VAULT_PASSWORD_ALL') != "":
+        envvar_vault_pass = "VAULT_PASSWORD_ALL"
+    else:
+        envvar_vault_pass = "VAULT_PASSWORD_BUILDENV"
+elif args.vault_id == "all":
     envvar_vault_pass = "VAULT_PASSWORD_ALL"
 else:
-    # envvar_vault_pass = "VAULT_PASSWORD_" + args.vault_id.upper()
     envvar_vault_pass = "VAULT_PASSWORD_BUILDENV"
 
-if os.getenv(envvar_vault_pass) is not None:
+if os.environ.get(envvar_vault_pass) is not None and os.environ.get(envvar_vault_pass) != "":
     print os.environ[envvar_vault_pass]
 else:
     print "ERROR: '" + envvar_vault_pass + "' is not set in environment"
