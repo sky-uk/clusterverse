e67f8ed5f0d04946a45ea5547bf8b6822c224cac
Dougal Seeley <dougal.seeley@sky.uk>
2018-11-21 14:45:03 +0000

    Rename host_vars, add owner to tags, python 3 default + Rename host_vars to hosttypevars + Add owner to instance tags. + Removed devkey UTF8 encoding + Better compatibility for public/private IPs + add vpc_name variable

--- .vaultpass-client.py
+++ .vaultpass-client.py
@@ -18,7 +18,7 @@ else:
     envvar_vault_pass = "VAULT_PASSWORD_BUILDENV"
 
 if os.environ.get(envvar_vault_pass) is not None and os.environ.get(envvar_vault_pass) != "":
-    print os.environ[envvar_vault_pass]
+    print (os.environ[envvar_vault_pass])
 else:
-    print "ERROR: '" + envvar_vault_pass + "' is not set in environment"
+    print ("ERROR: '" + envvar_vault_pass + "' is not set in environment")
     sys.exit(1)
