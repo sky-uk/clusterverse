98d656fd19cdd61970184f711a39f757293e63c9
Dougal Seeley <dougal.seeley@sky.uk>
2019-10-17 13:05:09 +0100

    Refactor disks_auto code, remove clusterid variable + Simplification of the disks_auto code.  Move fstype. + Remove the 'name' field from auto_volumes, and derive it from the mountpoint.  Saves duplicating information. + Fix issue with selecting non-disk partitions (e.g. CDRom). + Removed clusterid variable in volume creation + Change cluster_vars[clusterid] to cluster_vars. + Remove requirement for VAULT_PASSWORD_ALL from .vaultpass-client.py + Remove unused 'dynamic_inventory_dict' + Fix deprecation warning 'force_valid_group_names' + Allow cluster.yml to run without depending on clusterbuild role.

--- .vaultpass-client.py
+++ .vaultpass-client.py
@@ -1,21 +1,15 @@
 #!/usr/bin/env python
 import os
 import sys
-import argparse
+# import argparse
 
-parser = argparse.ArgumentParser()
-parser.add_argument("--vault-id", type=str)
-args = parser.parse_args()
+# parser = argparse.ArgumentParser()
+# parser.add_argument("--vault-id", type=str)
+# args = parser.parse_args()
 
-if args.vault_id is None or args.vault_id == "None" or args.vault_id == "default":
-    if os.environ.get('VAULT_PASSWORD_ALL') is not None and os.environ.get('VAULT_PASSWORD_ALL') != "":
-        envvar_vault_pass = "VAULT_PASSWORD_ALL"
-    else:
-        envvar_vault_pass = "VAULT_PASSWORD_BUILDENV"
-elif args.vault_id == "all":
-    envvar_vault_pass = "VAULT_PASSWORD_ALL"
-else:
-    envvar_vault_pass = "VAULT_PASSWORD_BUILDENV"
+# if args.vault_id is None or args.vault_id == "None" or args.vault_id == "default":
+
+envvar_vault_pass = "VAULT_PASSWORD_BUILDENV"
 
 if os.environ.get(envvar_vault_pass) is not None and os.environ.get(envvar_vault_pass) != "":
     print (os.environ[envvar_vault_pass])
