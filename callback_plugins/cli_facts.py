b82dcb9e3062e4a3af45669af53d430d031aac28
Dougal Seeley <dougal.seeley@sky.uk>
2019-12-09 10:39:09 +0000

    Fix callback_plugins/cli_facts.py + Satisfy new requirement for the Ansible 2.9 callback_plugin: the set_options function must be present.  Also fix a syntax 'error' that was valid in previous version of Ansible/ Python.

--- callback_plugins/cli_facts.py
+++ callback_plugins/cli_facts.py
@@ -5,7 +5,7 @@ from __future__ import absolute_import, division, print_function
 
 __metaclass__ = type
 
-ANSIBLE_METADATA = {'metadata_version': '1.1',
+ANSIBLE_METADATA = {'metadata_version': '1.2',
                     'status': ['preview'],
                     'supported_by': 'community'}
 
@@ -34,16 +34,30 @@ class CallbackModule(CallbackBase):
     CALLBACK_VERSION = 2.0
     CALLBACK_TYPE = 'aggregate'
     CALLBACK_NAME = 'cli_facts'
+    CALLBACK_NEEDS_WHITELIST = False
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
+        super(CallbackModule, self).__init__(*args, **kwargs)
         self._cliargs = CLIARGS
         self._argv = sys.argv
+        self._play = ""
+
+    def _all_vars(self, host=None, task=None):
+        # host and task need to be specified in case 'magic variables' (host vars, group vars, etc) need to be loaded as well
+        return self._play.get_variable_manager().get_vars(play=self._play, host=host, task=task)
+
+    def set_options(self, task_keys=None, var_options=None, direct=None):
+        super(CallbackModule, self).set_options(task_keys=task_keys, var_options=var_options, direct=direct)
+        pass
 
     def v2_playbook_on_play_start(self, play):
+        self._play = play
         variable_manager = play.get_variable_manager()
-        variable_manager.set_host_variable(variable_manager._inventory.localhost, "cliargs", self._cliargs)
-        variable_manager.set_host_variable(variable_manager._inventory.localhost, "argv", self._argv)
+        variable_manager.set_host_variable(str(variable_manager._inventory.localhost), "cliargs", self._cliargs)
+        variable_manager.set_host_variable(str(variable_manager._inventory.localhost), "argv", self._argv)
+        # variable_manager.set_host_facts(str(variable_manager._inventory.localhost), "argv", self._argv)
+
         hosts = variable_manager._inventory.get_hosts()
         for host in hosts:
-            variable_manager.set_host_variable(host, "cliargs", self._cliargs)
-            variable_manager.set_host_variable(host, "argv", self._argv)
+            variable_manager.set_host_variable(str(host), "cliargs", self._cliargs)
+            variable_manager.set_host_variable(str(host), "argv", self._argv)
