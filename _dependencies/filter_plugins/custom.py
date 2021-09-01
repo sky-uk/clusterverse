d5b112607f23590b34660a7d60e5b1b7c4b038d1
Dougal Seeley <dougal.seeley@sky.uk>
2021-09-01 10:35:57 +0100

    Allow automatic selection of latest cloud OS image; add ARM64 support. + Allow automatic selection of latest base image by a location filter for AWS and GCP + Adds image to cluster_hosts_target, because it is now hosttype-specific (for AWS). + If a cluster already exists (e.g. we are adding or fixing a node), look-up the base image for existing cluster, to prevent added cluster members from having different base images. + Add support for ARM64 flavoured images (AWS only - no GCP ARM images available yet).   + AWS AMIs are defined specific to the processor architecture, so:   + Add a new library: "ec2_instance_type_info.py", which calls the boto3 describe_instance_types API to get the architecture for a given flavor.   + Use this information to find the latest image for that architecture.   + Update metricbeat/filebeat/prometheus_node_exporter to select correct ARM64 packages (ARM packages exist for beats >7.12).

--- _dependencies/filter_plugins/custom.py
+++ _dependencies/filter_plugins/custom.py
@@ -39,33 +39,10 @@ def iplookup(fqdn):
         import dns.resolver
         return to_text(dns.resolver.query(fqdn, 'A')[0])
 
-# Returns a json object from a loosely defined string (e.g. encoded using single quotes instead of double), or an object containing "AnsibleUnsafeText"
-def json_loads_loose(inStr):
-    import re, json, sys
-
-    display.vv(u"json_loads_loose - input type: %s; value %s" % (type(inStr), inStr))
-    if type(inStr) is dict or type(inStr) is list:
-        json_object = json.loads((to_text(json.dumps(inStr))).encode('utf-8'))
-    else:
-        try:
-            json_object = json.loads(inStr)
-        except (ValueError, AttributeError, TypeError) as e:
-            try:
-                json_object = json.loads(to_text(re.sub(r'\'(.*?)\'([,:}])', r'"\1"\2', inStr).replace(': True', ': "True"').replace(': False', ': "False"')).encode('utf-8'))
-            except (ValueError, AttributeError, TypeError) as e:
-                display.warning(u"json_loads_loose - WARNING: could not parse attribute string (%s) as json: %s" % (to_native(inStr), to_native(e)))
-                return inStr
-        except:
-            e = sys.exc_info()[0]
-            display.warning(u"json_loads_loose - WARNING: could not parse attribute string (%s) as json: %s" % (to_native(inStr), to_native(e)))
-            return inStr
-    return json_object
-
 
 class FilterModule(object):
     def filters(self):
         return {
             'dict_agg': dict_agg,
-            'iplookup': iplookup,
-            'json_loads_loose': json_loads_loose
+            'iplookup': iplookup
         }
